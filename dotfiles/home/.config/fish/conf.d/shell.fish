0814672a85e14cd3ac5c905deb24774fd307e419
Jack Hayhurst <jhayhurst@liquidweb.com>
2021-03-08 12:25:49 -0500

    switched to brew leaves, and updating stuff
    Signed-off-by: Jack Hayhurst <jhayhurst@liquidweb.com>

--- dotfiles/home/.config/fish/conf.d/shell.fish
+++ dotfiles/home/.config/fish/conf.d/shell.fish
@@ -1,11 +1,11 @@
 
 contains $fish_user_paths ~/bin; or set -Ua fish_user_paths ~/bin
 
-export LESSOPEN="|/usr/local/bin/lesspipe.sh %s" LESS_ADVANCED_PREPROCESSOR=1
-
 contains $CDPATH . ; or set -Ua CDPATH .
 contains $CDPATH ~/src; or set -Ua CDPATH ~/src
 contains $CDPATH ~ ; or set -Ua CDPATH ~
+
+export EDITOR="vim"
 
 #alias openssl="docker run -t --rm rnix/openssl-gost /usr/bin/openssl"
 
