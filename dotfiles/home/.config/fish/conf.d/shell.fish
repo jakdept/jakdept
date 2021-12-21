9658dee43431441f72bf3ccb69bf5208a1bd95d4
Jack Hayhurst <jhayhurst@liquidweb.com>
2021-12-21 17:09:19 -0500

    fixed a fish autocomplete bug, i did a dumb
    Signed-off-by: Jack Hayhurst <jhayhurst@liquidweb.com>

--- dotfiles/home/.config/fish/conf.d/shell.fish
+++ dotfiles/home/.config/fish/conf.d/shell.fish
@@ -2,7 +2,7 @@
 contains $fish_user_paths ~/bin; or set -Ua fish_user_paths ~/bin
 contains $fish_user_paths /usr/local/sbin; or set -Ua fish_user_paths /usr/local/sbin
 
-contains $CDPATH . ; or set -Ua CDPATH .
+# contains $CDPATH . ; or set -Ua CDPATH .
 contains $CDPATH ~/src; or set -Ua CDPATH ~/src
 contains $CDPATH ~ ; or set -Ua CDPATH ~
 
