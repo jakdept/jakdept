0a090de0431c90fa177f9e634d02e937ba56481f
Jack Hayhurst <jhayhurst@liquidweb.com>
2022-03-11 10:53:02 -0500

    disabled ls pretty thingy

--- dotfiles/home/.config/fish/conf.d/ls.fish
+++ dotfiles/home/.config/fish/conf.d/ls.fish
@@ -1,12 +1,12 @@
 
 #alias ls="logo-ls"
 
-function ls --wraps ls --description 'alias ls=logo-ls'
-	if isatty stdout
-		logo-ls $argv
-	else
-		/usr/bin/env ls $argv
-	end
-end
+# function ls --wraps ls --description 'alias ls=logo-ls'
+# 	if isatty stdout
+# 		logo-ls $argv
+# 	else
+# 		/usr/bin/env ls $argv
+# 	end
+# end
 
 
