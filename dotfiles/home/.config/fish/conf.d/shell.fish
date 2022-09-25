2d9cb5c73f4359c5e0ce25324f64ba4720815dcc
Jack Hayhurst <jhayhurst@liquidweb.com>
2022-09-24 20:15:11 -0400

    incremental changes

--- dotfiles/home/.config/fish/conf.d/shell.fish
+++ dotfiles/home/.config/fish/conf.d/shell.fish
@@ -3,8 +3,8 @@ contains $fish_user_paths ~/bin; or set -Ua fish_user_paths ~/bin
 contains $fish_user_paths /usr/local/sbin; or set -Ua fish_user_paths /usr/local/sbin
 
 # contains $CDPATH . ; or set -Ua CDPATH .
-contains $CDPATH ~/src; or set -Ua CDPATH ~/src
-contains $CDPATH ~ ; or set -Ua CDPATH ~
+contains ~/src $CDPATH ; or set -Ua CDPATH ~/src
+contains ~ $CDPATH ; or set -Ua CDPATH ~
 
 export EDITOR="vim"
 
