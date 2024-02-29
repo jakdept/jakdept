6213c0ed4bcd77c79ae64c20fc7516c4220e8fb9
Jack Hayhurst <jhayhurst@liquidweb.com>
2024-02-28 22:37:17 -0500

    modifying dotfiles
    Signed-off-by: Jack Hayhurst <jhayhurst@liquidweb.com>

--- dotfiles/home/.config/fish/conf.d/go.fish
+++ dotfiles/home/.config/fish/conf.d/go.fish
@@ -6,7 +6,7 @@ function update-go
 	sudo rm -rf /usr/local/go
 	lynx -dump -listonly -nonumbers https://golang.org/dl |
 	grep (uname -s|tr '[:upper:]' '[:lower:]') |
-	grep amd64 |
+        grep (uname -m|sed 's/x86_64/amd64/') |
 	grep tar.gz |
 	head -n1 |
 	xargs curl -sSL -o- |
