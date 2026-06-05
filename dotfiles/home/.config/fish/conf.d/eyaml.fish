5c8a57009b09c3a157c6225964974ac8155f3faf
Jack Hayhurst <jhayhurst@liquidweb.com>
2026-06-05 13:03:04 -0400

    adding eyaml encrypt / decrypt things back in

--- dotfiles/home/.config/fish/conf.d/eyaml.fish
+++ dotfiles/home/.config/fish/conf.d/eyaml.fish
@@ -1,1 +1,6 @@
 set -gx EYAML_CONFIG ~/.eyaml/production.config.yaml
+
+alias eyaml-dev-encrypt="eyaml encrypt --pkcs7-public-key=$HOME/.eyaml/dev.public_key.pkcs7.pem"
+alias eyaml-dev-decrypt="eyaml decrypt --pkcs7-private-key=$HOME/.eyaml/dev.private_key.pkcs7.pem --pkcs7-public-key=$HOME/.eyaml/dev.public-key.pkcs7.pem"
+alias eyaml-staging-encrypt="eyaml encrypt --pkcs7-public-key=$HOME/.eyaml/staging.public_key.pkcs7.pem"
+alias eyaml-staging-decrypt="eyaml decrypt --pkcs7-private-key=$HOME/.eyaml/staging.private_key.pkcs7.pem --pkcs7-public-key=$HOME/.eyaml/staging.public_key.pkcs7.pem"
